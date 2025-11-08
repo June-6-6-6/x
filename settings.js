6e8994984e7e27ebbe4cae253b845eb9e0b043c9
June-6-6-6 <supreme2004june@gmail.com>
2025-11-08 22:05:23 +0300

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,22 @@
+
+/*━━━━━━━━━━━━━━━━━━━━*/
+/**
+*/
+/*━━━━━━━━━━━━━━━━━━━━*/
+
+const settings = {
+  packname: '',
+  author: '‎',
+  botName: "𝐉ᴜɴᴇ 𝐌ᴅ",
+  botOwner: 'supreme', // Your name
+  ownerNumber: '254792021944', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  maxStoreMessages: 20, 
+  storeWriteInterval: 10000,
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "2.6.2",
+  updateZipUrl: "https://github.com/vinpink2/june-private-repohide/archive/refs/heads/main.zip",
+};
+
+module.exports = settings;
