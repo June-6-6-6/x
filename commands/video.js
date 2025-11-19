251453327334c03c10fdb5c5033fab650598da27
June-6-6-6 <supreme2004june@gmail.com>
2025-11-19 08:45:04 +0300

    Update video.js

--- commands/video.js
+++ commands/video.js
@@ -118,7 +118,7 @@ async function videoCommand(sock, chatId, message) {
             video: { url: videoData.download },
             mimetype: 'video/mp4',
             fileName: `${videoData.title || videoTitle || 'video'}.mp4`,
-            caption: `${videoData.title || videoTitle || 'Video'}\n⬇️ DOWNLOAD BY JUNE-X BOT\n📌 By Humans, For Humans!.`
+            caption: `${videoData.title || videoTitle || 'Video'}\n\n⬇️ DOWNLOAD BY JUNE-X BOT\n\n> 📌 By Humans, For Humans!.`
         }, { quoted: message });
 
         //react sucess
