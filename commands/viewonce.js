32a0fe6bbda3c1d5e09c7a50514e88230adfbe15
June-6-6-6 <supreme2004june@gmail.com>
2025-11-21 19:22:20 +0300

    Update viewonce.js

--- commands/viewonce.js
+++ commands/viewonce.js
@@ -1,49 +1,26 @@
 const { downloadContentFromMessage } = require('@whiskeysockets/baileys');
 
 async function viewonceCommand(sock, chatId, message) {
-    // Extract quoted message
+    // Extract quoted imageMessage or videoMessage from your structure
     const quoted = message.message?.extendedTextMessage?.contextInfo?.quotedMessage;
     const quotedImage = quoted?.imageMessage;
     const quotedVideo = quoted?.videoMessage;
 
-    // Get sender JID (user.id)
-    const senderId = sock.user.id;
-
-    // Helper to download media
-    async function downloadMedia(msg, type) {
-        const stream = await downloadContentFromMessage(msg, type);
+    if (quotedImage && quotedImage.viewOnce) {
+        // Download and send the image
+        const stream = await downloadContentFromMessage(quotedImage, 'image');
         let buffer = Buffer.from([]);
         for await (const chunk of stream) buffer = Buffer.concat([buffer, chunk]);
-        return buffer;
-    }
-
-    if (quotedImage && quotedImage.viewOnce) {
-        const buffer = await downloadMedia(quotedImage, 'image');
-        const caption = quotedImage.caption || '';
-
-        // Send privately to sender
-        if (senderId) {
-            await sock.sendMessage(senderId, { image: buffer, fileName: 'media.jpg', caption }, { quoted: message });
-        }
-
-        // Send anti-viewonce in public chat
-        await sock.sendMessage(chatId, { image: buffer, fileName: 'media.jpg', caption }, { quoted: message });
-
+        await sock.sendMessage(chatId, { image: buffer, fileName: 'media.jpg', caption: quotedImage.caption || '\n\n🟢 RETRIVED BY JUNE-X' }, { quoted: message });
     } else if (quotedVideo && quotedVideo.viewOnce) {
-        const buffer = await downloadMedia(quotedVideo, 'video');
-        const caption = quotedVideo.caption || '';
-
-        // Send privately to sender
-        if (senderId) {
-            await sock.sendMessage(senderId, { video: buffer, fileName: 'media.mp4', caption }, { quoted: message });
-        }
-
-        // Send anti-viewonce in public chat
-        await sock.sendMessage(chatId, { video: buffer, fileName: 'media.mp4', caption }, { quoted: message });
-
+        // Download and send the video
+        const stream = await downloadContentFromMessage(quotedVideo, 'video');
+        let buffer = Buffer.from([]);
+        for await (const chunk of stream) buffer = Buffer.concat([buffer, chunk]);
+        await sock.sendMessage(chatId, { video: buffer, fileName: 'media.mp4', caption: quotedVideo.caption || '' }, { quoted: message });
     } else {
         await sock.sendMessage(chatId, { text: '❌ Please reply to a view-once image or video.' }, { quoted: message });
     }
 }
 
-module.exports = viewonceCommand;
+module.exports = viewonceCommand; 
