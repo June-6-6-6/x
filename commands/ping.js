0d6a8e44e341ea5e381041331b4126957b5268a0
June-6-6-6 <supreme2004june@gmail.com>
2025-11-22 16:11:01 +0300

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,22 +1,23 @@
 /*by supreme*/
 
-
 const os = require('os');
 const settings = require('../settings.js');
 
-async function pingCommand( sock, chatId, message ) {
+async function pingCommand(sock, chatId, message) {
   try {
     const start = Date.now();
     const sentMsg = await sock.sendMessage(chatId, {
-      text: '*🔹pong!...*'},{ quoted: message
-    });
+      text: '*🔹pong!...*'}, { quoted: message }
+    );
 
     const ping = Date.now() - start;
-    const response = `*🔸 June-X Speed: ${ping} ms*`;
+    // Format ping with decimal points
+    const formattedPing = ping.toFixed(2);
+    const response = `🔸 *June-X Speed: ${formattedPing} ms*`;
 
     await sock.sendMessage(chatId, {
       text: response,
-      edit: sentMsg.key// Edit the original message
+      edit: sentMsg.key // Edit the original message
     });   
     
   } catch (error) {
@@ -26,21 +27,3 @@ async function pingCommand( sock, chatId, message ) {
 }
 
 module.exports = pingCommand;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
