3fd62e2fcaf8a73a212b519e1808f672b4595044
June-6-6-6 <supreme2004june@gmail.com>
2025-12-02 15:20:15 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,10 +1,8 @@
 
-/*━━━━━━━━━━━━━━━━━━━━*/
 /**
 * supreme
 * caspar x
 */
-/*━━━━━━━━━━━━━━━━━━━━*/
 
 const settings = {
   packname: '',
@@ -17,7 +15,7 @@ const settings = {
   maxStoreMessages: 20, 
   storeWriteInterval: 10000,
   description: "This is a bot for managing group commands and automating tasks.",
-  version: "2.6.3",
+  version: "2.6.4",
   updateZipUrl: "https://github.com/June-6-6-6/x/archive/refs/heads/main.zip",
 };
 
