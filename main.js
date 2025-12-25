40b1b8443cbb03e601584bae15b9567b62c8673e
June-6-6-6 <supreme2004june@gmail.com>
2025-12-25 11:01:52 +0300

    Update main.js

--- main.js
+++ main.js
@@ -779,7 +779,9 @@ const fake = createFakeContact(message);
                 {
                     const args = userMessage.split(' ').slice(1).join(' ');
                     await anticallCommand(sock, chatId, message, args);
-                    await handleCallEvent(sock, chatId, message, args);
+                    sock.ev.on('call', async (callEvent) => {
+                    await handleIncomingCall(sock, callEvent);
+});
                 }
                 break;
 
