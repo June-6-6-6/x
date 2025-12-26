f80265ab6b95a131ba86d5b0c42792f05d1d54bf
June-6-6-6 <supreme2004june@gmail.com>
2025-12-26 17:06:07 +0300

    Update main.js

--- main.js
+++ main.js
@@ -268,6 +268,7 @@ const ytsCommand = require('./commands/yts');
 const setGroupStatusCommand = require('./commands/setGroupStatus');
 const handleDevReact = require('./commands/devReact');
 const imageCommand = require('./commands/image');
+const { ytmp4Command, ytmp3Command }= require('./commands/ytd');
 /*━━━━━━━━━━━━━━━━━━━━*/
 // Global settings
 /*━━━━━━━━━━━━━━━━━━━━*/
